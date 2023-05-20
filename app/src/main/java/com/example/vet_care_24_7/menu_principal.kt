1179ee90a0ac696f84e1615b4409099d3bd3527e
jesusdme2002 <jdmolinae@hotmail.com>
2023-05-19 19:00:01 -0500

    6:59

---
+++ app/src/main/java/com/example/vet_care_24_7/menu_principal.kt
@@ -0,0 +1,11 @@
+package com.example.vet_care_24_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu_principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_principal)
+    }
+}
